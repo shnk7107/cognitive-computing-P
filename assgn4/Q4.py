2878e50d5664edfced8556acf92fcc6f4e189b63
Shashank Tadikamalla <stadikamalla_be23@thapar.edu>
2025-01-30 16:03:30 +0530

    Update Q4.py

--- assgn4/Q4.py
+++ assgn4/Q4.py
@@ -3,37 +3,37 @@
 
 import numpy as np
 
-Your_Name=np.linspace(10,100,25)
-print(f"Array formed is : \n {Your_Name}")
+shashank=np.linspace(10,100,25)
+print(f"Array formed is : \n {shashank}")
 print('\n')
 
 
 # Print the dimensions of the array,shape, total elements, the data type of each element 
 # and total number of bytes consumed by the array. 
 
-print(f"Dimentions of the array is : {Your_Name.ndim}")
+print(f"Dimentions of the array is : {shashank.ndim}")
 print('\n')
 
-print(f"Shape of the array is : {Your_Name.shape}")
+print(f"Shape of the array is : {shashank.shape}")
 print('\n')
 
-print(f"Size of the array is : {Your_Name.size}")
+print(f"Size of the array is : {shashank.size}")
 print('\n')
 
-print(f"Datatype of each element of the array is : {Your_Name.dtype}")
+print(f"Datatype of each element of the array is : {shashank.dtype}")
 print('\n')
 
-print(f"Total number of bytes consumed by the array is : {Your_Name.nbytes}")
+print(f"Total number of bytes consumed by the array is : {shashank.nbytes}")
 print('\n')
 
 # Find the transpose of this array using reshape() attribute. 
 
-Your_Name_reshaped=Your_Name.reshape(25,1)
-print(f"transpose of the array is : {Your_Name_reshaped}")
+shashank_reshaped=shashank.reshape(25,1)
+print(f"transpose of the array is : {shashank_reshaped}")
 print('\n')
 
 
 # Can we do the same with T attribute?
 
-print(f"transpose of the array is : {Your_Name_reshaped.T}")
+print(f"transpose of the array is : {shashank_reshaped.T}")
 print('\n')
